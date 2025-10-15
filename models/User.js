da9a89db6b315018dcc72e3322ad066ab58ac71c
murilo <ninezconta@gmail.com>
2025-10-15 01:00:22 -0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  passwordHash: { type: String, required: true },
+  verified: { type: Boolean, default: false },
+  verificationCode: String,
+  verificationExpires: Date
+});
+
+module.exports = mongoose.model('User', userSchema);
 
